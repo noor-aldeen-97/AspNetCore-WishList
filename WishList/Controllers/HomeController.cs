a7f3f817e11d02b8ccb12b9ebaca5b1c021319b1
noor-aldeen-97 <47923363+noor-aldeen-97@users.noreply.github.com>
2020-10-11 22:40:08 +0300

    Update

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WishList
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: HomeController
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Error()
         {
-            return View();
+            return View("Error");
         }
     }
 }
